05628c285d1734c232d27286d4fa90fd648cc706
Sam Harwell <sam@tunnelvisionlabs.com>
2018-05-01 10:46:38 -0500

    Add WpfFactAttribute from Roslyn

---
+++ Tvl.VisualStudio.MouseFastScroll.IntegrationTests/Threading/WpfTestCase.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Roslyn.Test.Utilities
+{
+    public sealed class WpfTestCase : XunitTestCase
+    {
+        public WpfTestSharedData SharedData { get; private set; }
+ 
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [Obsolete("Called by the de-serializer; should only be called by deriving classes for de-serialization purposes")]
+        public WpfTestCase()
+        {
+        }
+ 
+        public WpfTestCase(IMessageSink diagnosticMessageSink, TestMethodDisplay defaultMethodDisplay, ITestMethod testMethod, object[] testMethodArguments = null)
+            : base(diagnosticMessageSink, defaultMethodDisplay, testMethod, testMethodArguments)
+        {
+            SharedData = WpfTestSharedData.Instance;
+        }
+ 
+        public override Task<RunSummary> RunAsync(IMessageSink diagnosticMessageSink, IMessageBus messageBus, object[] constructorArguments, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource)
+        {
+            var runner = new WpfTestCaseRunner(SharedData, this, DisplayName, SkipReason, constructorArguments, TestMethodArguments, messageBus, aggregator, cancellationTokenSource);
+            return runner.RunAsync();
+        }
+ 
+        public override void Deserialize(IXunitSerializationInfo data)
+        {
+            base.Deserialize(data);
+            SharedData = WpfTestSharedData.Instance;
+        }
+    }
+}
