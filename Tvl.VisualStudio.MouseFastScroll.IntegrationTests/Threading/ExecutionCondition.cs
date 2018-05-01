d9753b10575a01e193171f04002c17aff9631be8
Sam Harwell <sam@tunnelvisionlabs.com>
2018-05-01 11:10:09 -0500

    Implement missing helper types

---
+++ Tvl.VisualStudio.MouseFastScroll.IntegrationTests/Threading/ExecutionCondition.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE.txt in the project root for license information.
+
+namespace Tvl.VisualStudio.MouseFastScroll.IntegrationTests.Threading
+{
+    public abstract class ExecutionCondition
+    {
+        public abstract bool ShouldSkip { get; }
+
+        public abstract string SkipReason { get; }
+    }
+}
