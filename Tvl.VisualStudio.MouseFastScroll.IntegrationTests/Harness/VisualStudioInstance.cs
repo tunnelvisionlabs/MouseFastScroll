1ffbb8069d6628baa8c46a682890a8b7e21702b5
Sam Harwell <sam@tunnelvisionlabs.com>
2018-06-24 17:58:33 -0500

    Implement debugger auto-attach
    A very big thank you to @josetr for the hints here.

--- Tvl.VisualStudio.MouseFastScroll.IntegrationTests/Harness/VisualStudioInstance.cs
+++ Tvl.VisualStudio.MouseFastScroll.IntegrationTests/Harness/VisualStudioInstance.cs
@@ -30,6 +30,16 @@ namespace Tvl.VisualStudio.MouseFastScroll.IntegrationTests.Harness
             SupportedPackageIds = supportedPackageIds;
             InstallationPath = installationPath;
 
+            if (Debugger.IsAttached)
+            {
+                // If a Visual Studio debugger is attached to the test process, attach it to the instance running
+                // integration tests as well.
+                var debuggerHostDte = GetDebuggerHostDte();
+                int targetProcessId = Process.GetCurrentProcess().Id;
+                var localProcess = debuggerHostDte?.Debugger.LocalProcesses.OfType<EnvDTE80.Process2>().FirstOrDefault(p => p.ProcessID == hostProcess.Id);
+                localProcess?.Attach2("Managed");
+            }
+
             StartRemoteIntegrationService(dte);
 
             string portName = $"IPC channel client for {HostProcess.Id}";
@@ -115,6 +125,21 @@ namespace Tvl.VisualStudio.MouseFastScroll.IntegrationTests.Harness
             => _inProc.GetErrorListErrorCount();
 
         public bool IsRunning => !HostProcess.HasExited;
+
+        private static DTE GetDebuggerHostDte()
+        {
+            var currentProcessId = Process.GetCurrentProcess().Id;
+            foreach (var process in Process.GetProcessesByName("devenv"))
+            {
+                var dte = IntegrationHelper.TryLocateDteForProcess(process);
+                if (dte?.Debugger?.DebuggedProcesses?.OfType<EnvDTE.Process>().Any(p => p.ProcessID == currentProcessId) ?? false)
+                {
+                    return dte;
+                }
+            }
+
+            return null;
+        }
 
         public void ExecuteInHostProcess(Type type, string methodName)
         {
