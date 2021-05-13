f02e70ecacd372d2d5fec254e610d4dad4e2ef8b
Sam Harwell <Sam.Harwell@microsoft.com>
2021-05-13 07:27:22 -0700

    Update Visual Studio 2022 support

--- Tvl.VisualStudio.MouseFastScroll.17/Properties/AssemblyInfo.cs
+++ Tvl.VisualStudio.MouseFastScroll.17/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE.txt in the project root for license information.
 
 using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -13,3 +14,5 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+[assembly: InternalsVisibleTo("Tvl.VisualStudio.MouseFastScroll.UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001003d5ffdd6b31bbb97586ba049c04d02ee30d40fa7ce34d4cfadc9ea1a1cc9b02e80cbf8c50b4a428ad6839a25902cde0bf0059ae8afcb90e0ac863c6768d4d9d91ba0f6c96425299d92cc9e89e0575149bf4ac3fd223cec9bf176ce764a346d1b99a831cc5b51585bd72e3a9c478c358e79a6563ea654b0bef84a0ec2bd9567e9")]
